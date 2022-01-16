2898e0cc92b350022424b7ed4a067aef82c27bc4
TuemoT <thomastuemo@yahoo.fr>
2022-01-16 13:50:36 +0100

    updated

---
+++ src/app/patient.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Patient } from './patient';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+
+  private baseURL = "http://localhost:8082/api/v3/patients";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPatientsList(): Observable<Patient[]>{
+    return this.httpClient.get<Patient[]>(`${this.baseURL}`);
+  }
+
+  createPatient(patient: Patient): Observable<Object>{
+    return this.httpClient.post(`${this.baseURL}`, patient);
+  }
+  getPatients(id: number): Observable<any> {
+    return this.httpClient.get(`${this.baseURL}/${id}`);
+  }
+  getPatientById(id: number): Observable<Patient>{
+    return this.httpClient.get<Patient>(`${this.baseURL}/${id}`);
+  }
+  getPatient(id: number): Observable<any> {
+    return this.httpClient.get(`${this.baseURL}/${id}`);
+  }
+  updatePatient(id: number, patient: Patient): Observable<Object>{
+    return this.httpClient.put(`${this.baseURL}/${id}`, patient);
+  }
+
+  deletePatient(id: number): Observable<Object>{
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
+  }
+  findByLastName(lastName) {
+    return this.httpClient.get(`${this.baseURL}?lastName=${lastName}`);
+  }
+
+}
